4fb8abaf23930f330bc838d1dfcab048ab11d7ef
Shubham-Kant <pro.shubham1999@gmail.com>
2019-06-25 19:22:05 +0530

    express and view engine are set up and home page is rendered

---
+++ index.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const port=8000;
+const app=express();
+
+app.use('/',require('./routes'));
+app.set('view engine','ejs');
+app.set('views','./views');
+
+app.listen(port,function(err){
+    if(err){
+        console.log(`error is : ${err}`);
+    }
+    console.log(`the port number is : ${port}`);
+});
