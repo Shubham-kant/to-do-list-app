a44d60015873faea59ed1583fc4ca0e816b19693
Shubham-Kant <pro.shubham1999@gmail.com>
2020-11-17 18:15:31 +0530

    made some url changes

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,6 @@
 const mongoose=require('mongoose');
 const url=process.env.MONGODB_URI || 'mongodb://localhost/todolistdevelopment';
-// mongoose.connect('mongodb+srv://heroku_wjl3mk00:rayoptics1@cluster-wjl3mk00.8ercl.mongodb.net/todolist_development?retryWrites=true&w=majority' || process.env.MONGODB_URI ||'mongodb://localhost/todolist_development');
-// mongoose.connect('mongodb+srv://heroku_wjl3mk00:rayoptics1@cluster-wjl3mk00.8ercl.mongodb.net/todolist_development?retryWrites=true&w=majority', {useNewUrlParser:true});
+
 mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology:true});
 const db=mongoose.connection;
 db.on('error',console.error.bind(console,'error in connecting to Mongodb'));
